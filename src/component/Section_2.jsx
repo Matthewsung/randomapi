59dd15be4be36c7f0fcbd995b3d8b82f9aae64b1
juyeongsung <jooyongs1@gmail.com>
2021-08-01 23:30:09 +0900

    8.1

--- src/component/Section_2.jsx
+++ src/component/Section_2.jsx
@@ -29,7 +29,6 @@ const Section_2 = () => {
       <div className="Sed-ut-perspiciatis-unde-omnis-iste-natus-error-sit-voluptatem-accusantium-doloremque-laudantium-to">
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.
       </div>
-      
      </div>
     </>
   )
